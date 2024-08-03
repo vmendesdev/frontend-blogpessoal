244f3014569dcb53b7cbb60b287ec7b382654025
vmendesdev <vmendesjob@gmail.com>
2024-08-03 17:56:01 -0300

    Configuração inicial do frontend do projeto blog pessoal com react e vite

---
+++ blog-pessoal/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Home from './paginas/Home';
+
+
+function App() {
+  return (
+   <Home />
+  );
+}
+
+export default App;
---
+++ blog-pessoal/src/paginas/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo' >Home</h1>
+            <img src="https://img.freepik.com/fotos-gratis/paisagem-de-desenho-animado-de-uma-ilha-tropical_23-2151724107.jpg?t=st=1722717880~exp=1722721480~hmac=510ccdc7661ca1c4d4bb0c922aad85c34b94430b292ba0502536ca99db2176b4&w=1060" alt="Imagem de fundo" className='img'/>
+           
+        </>
+    );
+}
+
+export default Home;
