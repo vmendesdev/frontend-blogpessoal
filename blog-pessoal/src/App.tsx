907708aa9d6143a22ed04e424df6275d682cbd53
vmendesdev <vmendesjob@gmail.com>
2024-08-05 14:19:25 -0300

    Estrutura do hook useState em React com TypeScript

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -3,9 +3,11 @@ import './App.css';
 import Home from './paginas/Home';
 
 function App() {
+ 
   return (
-   <Home  title="Componente Home"
-          description="Este é um componente Home que recebe props."/>
+    <>
+      <Home/>
+    </>
   );
 }
 
