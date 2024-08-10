4ffd2c418c4fc6a6411d8b0ac7b6e672f6d126d1
Vanessa Mendes <vmendesjob@gmail.com>
2024-08-10 16:52:24 -0300

    verificando bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import './App.css';
-import Home from './paginas/home/Home';
-import Navbar from './components/navbar/Navbar';
+import React from 'react'
+import './App.css'
+import Home from './paginas/home/Home'
+import Navbar from './components/navBar/NavBar'
 import Footer from './components/footer/Footer';
 
 function App() {
