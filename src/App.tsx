b64b910df7d571f98361d10f86fda1c001e70ce3
Vanessa Mendes <vmendesjob@gmail.com>
2024-08-11 17:05:13 -0300

    atualização do link

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 
-import Navbar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/login/Login';
@@ -9,6 +8,7 @@ import Cadastro from './paginas/cadastro/Cadastro';
 import Home from './paginas/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import NavBar from './components/navbar/NavBar';
 
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
     <>
     <AuthProvider>
         <BrowserRouter>
-          <Navbar />
+          <NavBar />
           <div className='min-h-[80vh]'>
             <Routes>
               <Route path="/" element={<Login />} />
