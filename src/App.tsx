8959a6964a400ef175a80502d1360e24444f00b2
Vanessa Mendes <vmendesjob@gmail.com>
2024-08-11 14:54:43 -0300

    Construindo o template do componente Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import './App.css';
 
-import NavBar from './components/navbar/NavBar';
+import Navbar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/login/Login';
 import Cadastro from './paginas/cadastro/Cadastro';
 import Home from './paginas/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
 
 
 function App() {
   return (
     <>
-   
+    <AuthProvider>
         <BrowserRouter>
           <NavBar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Cadastro />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
@@ -25,7 +26,7 @@ function App() {
           </div>
           <Footer />
         </BrowserRouter>
-  
+        </AuthProvider>
     </>
   );
 }
