adc5e8269ba9900d3cafb91aae0468bf94189c81
Vanessa Mendes <vmendesjob@gmail.com>
2024-08-12 16:45:14 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
 import FormularioTema from './components/temas/formularioTema/FormularioTema';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
 
 
 function App() {
@@ -18,7 +19,7 @@ function App() {
     <>
     <AuthProvider>
         <BrowserRouter>
-          <Navbar />
+          <NavBar />
           <div className='min-h-[80vh]'>
             <Routes>
               <Route path="/" element={<Login />} />
@@ -29,6 +30,7 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
             </Routes>
           </div>
           <Footer />
