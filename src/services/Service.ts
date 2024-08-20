1de01b9f75ec4c2ceb2be1b73c94a163800b68f1
vmendesdev <vmendesjob@gmail.com>
2024-08-20 16:43:27 -0300

    atualização url backend

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: 'https://blogpessoal-fsz1.onrender.com'
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
